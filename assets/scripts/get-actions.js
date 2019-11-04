94d5c54c76b11a1890f0d7b606cf67a537bf1eb5
Tihomir Selak <tihomir.selak@infinum.hr>
2019-11-04 10:19:42 +0100

    Add documentation examples

--- assets/scripts/get-actions.js
+++ assets/scripts/get-actions.js
@@ -1,9 +1,25 @@
 import { ucfirst } from './ucfirst';
 
 /**
- * This method is for atributes with property type `object` with any number of values, eg. `content`, `url`, `type` etc.
+ * This method is used when atributes have property type `object` with any number of values, eg. `content`, `url`, `type` etc.
  * This function generates callback for each of the values from attribute. Generate callback with name onChange${attribute_name}${propery_name}
- * Example `onChangeHeadingColor` where atribute name is `heading` and object property is `color`
+ * 
+ * Example: 
+ * "attributes": {
+ *    "heading": {
+ *       "type": "object",
+ *       "default": {
+ *         "content": "",
+ *         "color": "default",
+ *         "size": "default",
+ *         "type": "default",
+ *         "weight": "normal",
+ *         "family": "a1-serif"
+ *       },
+ *       "multipleProps": true
+ *     }
+ * }
+ * Inside actions there will be `onChangeHeadingColor` where atribute name is `heading` and object property is `color`
  *
  * @param {object} setAttributes Method for saving atributes.
  * @param {object} attributes All atributes from namifest.json
@@ -45,7 +61,7 @@ const multiplePropsActions = (setAttributes, attributes, key, propsAttributes) =
 };
 
 /**
- * This method is for atributes with single property.
+ * This method is used to set atributes with single property.
  * This function generates callback for that propertiy value.
  *
  * @param {object} setAttributes Method for saving atributes.
@@ -72,11 +88,26 @@ const siglePropsAction = (setAttributes, key) => {
 
 
 /**
- * This method is for media atributes, it is property type `object` with default values of `id`, `url`, `title`.
+ * This method is used for setting media attributes. It is property type `object` with default values of `id`, `url`, `title`.
  * This function generates callback that saves `id`, `url` and `title` of attribute.
+ * 
+ * Example: 
+ * "attributes": {
+ *   "primaryVideo": {
+ *     "type": "object",
+ *     "default": {
+ *       "id": 0,
+ *       "url": "",
+ *       "title": ""
+ *     },
+ *     "mediaAction": true
+ *   }
+ * }
  *
+ * Inside actions there will be `onChangePrimaryVideo` function that will update `id`, `url` and `title` and expect that a given object have those properties
+ * 
  * @param {object} setAttributes Method for saving atributes.
- * @param {string} key Came of the property in manifes.
+ * @param {string} key Came of the property in manifest.
  *
  * @since 1.0.5 Ucfirst path hotfix.
  * @since 1.0.4 Moving to scripts folder.
@@ -86,8 +117,8 @@ const siglePropsAction = (setAttributes, key) => {
 const mediaPropsAction = (setAttributes, key) => {
   const output = {};
 
-  // Set output as a object key with anonimus function callback.
-  // Keys first name must be uppercased.
+  // Set output as an object key with anonymous function callback.
+  // Keys name must be written in uppercase.
   output[`onChange${ucfirst(key)}`] = function(value) {
     setAttributes({
       [key]: {
@@ -105,12 +136,13 @@ const mediaPropsAction = (setAttributes, key) => {
 
 /**
  * Create attributes actions from blocks manifest.json.
+ * 
  * Actions are passed in child components in order to update props on event.
  * Default function output is `onChange` + attribute name.
  * Example `onChangeContent`.
  * Depending on prope
  *
- * @param {object} props Block props so we can get `setAttributes` Method.
+ * @param {object} props Block props so we can get `setAttributes` method.
  * @param {object} manifest Block manifest.json so we can get all attributes.
  *
  * @since 1.0.5 Ucfirst path hotfix.
